cc39b8af34692c14cecddabd6f360c1659aac0db
Adamya Agrawal <106526372+EnCoder101@users.noreply.github.com>
2024-02-14 23:38:15 +0530

    class lecture week-10.2

---
+++ week-10/class_lecture-2/dist/index.js
@@ -0,0 +1,50 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function insertUser(username, password, firstName, lastName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.create({
+            data: {
+                username,
+                password,
+                firstName,
+                lastName
+            },
+            select: {
+                id: true,
+                username: true
+            }
+        });
+        console.log(res);
+    });
+}
+function updateUser(username, { firstName, lastName }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.update({
+            where: {
+                username
+            },
+            data: {
+                firstName, lastName
+            },
+            select: {
+                username: true,
+                firstName: true,
+                lastName: true
+            }
+        });
+        console.log(res);
+    });
+}
+updateUser("mobu@gmail.com", { firstName: "mobu", lastName: "mobu" });
+// insertUser("mobu@gmail.com","12345","adamya","agrawal")
---
+++ week-10/class_lecture-2/src/index.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+  const res = await prisma.user.create({
+    data: {
+        username,
+        password,
+        firstName,
+        lastName
+    },
+    select:{
+        id: true,
+        username:true
+    }
+  })
+
+  console.log(res)
+}
+
+interface UpdateParams {
+    firstName : string
+    lastName : string
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+  const res = await prisma.user.update({
+    where:{
+        username
+    },
+    data:{
+        firstName,lastName
+    },
+    select:{
+        username:true,
+        firstName:true,
+        lastName:true
+
+    }
+  })
+  console.log(res)
+}
+updateUser("mobu@gmail.com",{firstName : "mobu", lastName : "mobu"})
+// insertUser("mobu@gmail.com","12345","adamya","agrawal")
