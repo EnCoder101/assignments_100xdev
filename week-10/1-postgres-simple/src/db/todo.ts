c740aa70450624212eae4ba6387eff7502677d69
Adamya Agrawal <106526372+EnCoder101@users.noreply.github.com>
2024-02-17 18:29:47 +0530

    Week 10.1 Assignment

--- week-10/1-postgres-simple/src/db/todo.ts
+++ week-10/1-postgres-simple/src/db/todo.ts
@@ -1,4 +1,4 @@
-import { client } from "..";
+import { client } from "../index";
 /*
  * Function should insert a new todo for this user
  * Should return a todo object
@@ -10,7 +10,22 @@ import { client } from "..";
  * }
  */
 export async function createTodo(userId: number, title: string, description: string) {
-    
+    try{
+        const insertQuery = `
+        INSERT INTO todos (
+            user_id,
+            title,
+            description
+            ) VALUES ($1,$2,$3)
+            RETURNING *
+        `
+        const values = [userId,title,description]
+        const createNewTodo  = await client.query(insertQuery,values);
+        return createNewTodo.rows[0];
+    }catch(err){
+        console.log("Some Error")
+        return err;
+    }
 }
 /*
  * mark done as true for this specific todo.
@@ -23,7 +38,20 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+    try{
+        const updateQuery = `
+            UPDATE todos 
+            SET done = true
+            WHERE id = $1
+            RETURNING * ;
+        `
+        const value = [todoId];
+        const updateThatTodo = await client.query(updateQuery,value);
+        return updateThatTodo.rows[0];
+    }catch(err){
+        console.log("Some Error")
+        return err;
+    }
 }
 
 /*
@@ -37,5 +65,16 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+    try{
+        const getTodoQuery = `
+            SELECT * FROM todos
+            WHERE user_id = $1; 
+        `
+        const value = [userId];
+        const getUserTodo = await client.query(getTodoQuery,value);
+        return Array.isArray(getUserTodo.rows) ? getUserTodo.rows : []; 
+    }catch(err){
+        console.log("Some Error",err)
+        return [];
+    }
 }
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -1,4 +1,4 @@
-import { client } from "..";
+import { client } from "../index";
 
 /*
  * Should insert into the users table
@@ -10,7 +10,22 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    try{
+        const createUserQuery = `
+            INSERT INTO users (
+                username,
+                password,
+                name
+            ) VALUES ($1,$2,$3) 
+        `;
+        const value = [username,password,name];
+        const createNewUser = await client.query(createUserQuery,value);
+        return createNewUser.rows[0]
+    }
+    catch(err){
+        console.log("Some Error")
+        return err
+    }
 }
 
 /*
@@ -22,5 +37,16 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    try{
+        const getUserQuery = `
+            Select * from users 
+            where id = $1
+        `
+        const userID = [userId];
+        const getThatUser = await client.query(getUserQuery,userID)
+        return getThatUser.rows[0];
+    }catch(err){
+        console.log("Some Error")
+        return err
+    }
 }
